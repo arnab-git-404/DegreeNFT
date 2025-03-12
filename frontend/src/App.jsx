de1309210f4a4c44978ad8f2c97031aa225d22ee
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-03-12 21:11:02 +0530

    update02

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { Layout } from "./components/Layout";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import AppRoutes from "./AppRoutes";
+
+const App = () => {
+  // Set up Solana testnet connection
+  const network = WalletAdapterNetwork.Testnet;
+  const endpoint = clusterApiUrl(network);
+  const wallets = [new PhantomWalletAdapter()];
+
+  return (
+    <>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <Router>
+              <Layout>
+                <AppRoutes />
+              </Layout>
+            </Router>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </>
+  );
+};
+
+export default App;
